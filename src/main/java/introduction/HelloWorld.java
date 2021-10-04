dd339a2f1478fd710ebf6c1da4c64739043e7994
barska2 <b.o.skalik@gmail.com>
2021-10-04 14:50:52 +0200

    pracownia programowania 2021.10.04

--- src/main/java/introduction/HelloWorld.java
+++ src/main/java/introduction/HelloWorld.java
@@ -1,8 +1,12 @@
 package introduction;
 
+import org.apache.log4j.Logger;
+
 public class HelloWorld {
 
     public static void main(String [ ] args) {
-        System.out.println("Hello! Witaj!");
-    }
+        Logger log = Logger.getLogger("name");
+        log.info("INFO.COM");
+        System.out.println("Hello! Witaj!");}
+
 }
